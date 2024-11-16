1a704d161c518f2a4997fb9f31e2dcbe2f14b7a2
Tim Graboski <whiterook6@gmail.com>
2024-11-15 23:04:14 -0800

    downloading multiple songs, and zipping them to the browser in one.

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,7 +1,8 @@
 import { spawn } from "child_process";
 
 export class DownloaderController {
-    // create an async generrator metod
+
+  /** Downloads music from this URL, either one song or many, and returns a list of filenames */
   public download = async function* (url: string) {
     const command = spawn('yt-dlp', [
       "-o", "./music/%(title)s.%(ext)s",
@@ -20,7 +21,16 @@ export class DownloaderController {
     });
 
     for await (const chunk of command.stdout) {
-      yield chunk;
+      const lines: string[] = chunk.toString().split("\n").filter(Boolean);
+      for (const line of lines) {
+        if (line.startsWith("[Metadata] Adding metadata to ")){
+          debugger;
+          const match = line.match(/\[Metadata\] Adding metadata to \"(.*)\"/);
+          if (match){
+            yield match[1];
+          }
+        }
+      }
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,90 @@
 import express, { Request, Response } from "express";
 import { DownloaderController } from "./Controller";
+import path from "path";
+import fs from "fs";
+import JSZip from "jszip";
 
 const server = express();
-server.get("/", async (request: Request, response: Response) => {
-  response.status(200);
-  const controller = new DownloaderController();
-  const downloadOutput = controller.download("https://soundcloud.com/auvicmusic/sets/voices-call-remastered");
-  for await (const chunk of downloadOutput) {
-    response.write(chunk);
+server.get("/api/download", async (request: Request, response: Response) => {
+  const url = request.query.url as string;
+  if (!url){
+    return response.status(400).setHeader("Content-Type", "application/json").send({
+      error: "URL is required"
+    });
   }
-  return response.end();
+
+  const controller = new DownloaderController();
+  let filenames: string[] = [];
+  try {
+    console.log(`Downloading from ${url}`);
+    const files = controller.download(url);
+    for await (const file of files) {
+      console.log(`Downloaded ${file} from ${url}`);
+      filenames.push(file);
+    }
+  } catch (error) {
+    console.error(error);
+    return response.status(500).setHeader("Content-Type", "application/json").send({
+      error: "An error occurred downloading files",
+      detail: error.message
+    });
+  };
+  
+  if (filenames.length === 0){
+    console.log(`No files downloaded from ${url}`);
+    return response.status(500).setHeader("Content-Type", "application/json").send({
+      error: "No files downloaded"
+    });
+  }
+  
+  if (filenames.length === 1){
+    try {
+      console.log(`Sending ${filenames[0]} from ${url}`);
+      return response.status(200).sendFile(path.join(__dirname, "..", filenames[0]));
+    } catch (error) {
+      return response.status(500).setHeader("Content-Type", "application/json").send({
+        error: "An error occurred downloading the song",
+        detail: error.message
+      });
+    }
+  } else {
+    let content;
+    try {
+      const zip = new JSZip();
+      console.log(`Zipping ${filenames.length} files from ${url}`);
+      for (const filename of filenames){
+        zip.file(filename, fs.readFileSync(path.join(__dirname, "..", filename)));
+      }
+
+      content = await zip.generateAsync({
+        type: "nodebuffer",
+        compression: "DEFLATE",
+        compressionOptions: {
+          level: 9
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).setHeader("Content-Type", "application/json").send({
+        error: "An error occurred creating the zip file",
+        detail: error.message
+      });
+    }
+    
+    try {
+      console.log(`Sending zip file from ${url}`);
+      response.setHeader("Content-Type", "application/zip");
+      response.setHeader("Content-Disposition", "attachment; filename=download.zip");
+      response.status(200).send(content);
+      console.log(`Sent zip file from ${url}`);
+    } catch (error){
+      console.error(error);
+      return response.status(500).setHeader("Content-Type", "application/json").send({
+        error: "An error occurred downloading the zip file",
+        detail: error.message
+      });
+    };
+  }
 });
 
 server.listen(3000, () => {
