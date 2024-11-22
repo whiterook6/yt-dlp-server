3e61d1f0c9be8f15cf5cd659269b5142e11b4b7f
Tim Graboski <whiterook6@gmail.com>
2024-11-22 09:26:48 -0800

    cleaning up error handling

--- src/index.ts
+++ src/index.ts
@@ -6,52 +6,44 @@ import JSZip from "jszip";
 
 const server = express();
 server.get("/api/download", async (request: Request, response: Response) => {
+  const sendError = (errorCode: number, message: string, detail?: string) => {
+    return response.status(errorCode).setHeader("Content-Type", "application/json").send({
+      error: message,
+      detail: detail
+    });
+  }
+
   const url = request.query.url as string;
   if (!url){
-    return response.status(400).setHeader("Content-Type", "application/json").send({
-      error: "URL is required"
-    });
+    return sendError(400, "URL is required");
   }
 
   const controller = new DownloaderController();
   let filenames: string[] = [];
   try {
-    console.log(`Downloading from ${url}`);
     const files = controller.download(url);
     for await (const file of files) {
-      console.log(`Downloaded ${file} from ${url}`);
       filenames.push(file);
     }
   } catch (error) {
     console.error(error);
-    return response.status(500).setHeader("Content-Type", "application/json").send({
-      error: "An error occurred downloading files",
-      detail: error.message
-    });
+    return sendError(500, "An error occurred downloading files");
   };
   
   if (filenames.length === 0){
-    console.log(`No files downloaded from ${url}`);
-    return response.status(500).setHeader("Content-Type", "application/json").send({
-      error: "No files downloaded"
-    });
+    return sendError(500, "No files downloaded");
   }
   
   if (filenames.length === 1){
     try {
-      console.log(`Sending ${filenames[0]} from ${url}`);
       return response.status(200).sendFile(path.join(__dirname, "..", filenames[0]));
     } catch (error) {
-      return response.status(500).setHeader("Content-Type", "application/json").send({
-        error: "An error occurred downloading the song",
-        detail: error.message
-      });
+      return sendError(500, "An error occurred downloading the song or songs");
     }
   } else {
     let content;
     try {
       const zip = new JSZip();
-      console.log(`Zipping ${filenames.length} files from ${url}`);
       for (const filename of filenames){
         zip.file(filename, fs.readFileSync(path.join(__dirname, "..", filename)));
       }
@@ -65,24 +57,16 @@ server.get("/api/download", async (request: Request, response: Response) => {
       });
     } catch (error) {
       console.error(error);
-      return response.status(500).setHeader("Content-Type", "application/json").send({
-        error: "An error occurred creating the zip file",
-        detail: error.message
-      });
+      return sendError(500, "An error occurred creating the zip file", error.message);
     }
     
     try {
-      console.log(`Sending zip file from ${url}`);
       response.setHeader("Content-Type", "application/zip");
       response.setHeader("Content-Disposition", "attachment; filename=download.zip");
       response.status(200).send(content);
-      console.log(`Sent zip file from ${url}`);
     } catch (error){
       console.error(error);
-      return response.status(500).setHeader("Content-Type", "application/json").send({
-        error: "An error occurred downloading the zip file",
-        detail: error.message
-      });
+      return sendError(500, "An error occurred downloading the zip file",error.message);
     };
   }
 });
